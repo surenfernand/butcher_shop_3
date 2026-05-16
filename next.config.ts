30b335812907e7a87aefb0e5b6c9d28de2804cde
surenfernand <fernandosuren6@gmail.com>
2026-05-17 00:54:14 +0530

    output: standalone fix

--- next.config.ts
+++ next.config.ts
@@ -69,7 +69,7 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: path.resolve(dirname),
   },
-  output: 'standalone',
+  // output: 'standalone',
 }
 
 export default withPayload(nextConfig)
